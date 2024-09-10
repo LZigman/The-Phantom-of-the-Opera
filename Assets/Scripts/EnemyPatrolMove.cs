ee229f87e3191acb32cc358d81fecd7b1fa726b8
laufer2805 <p.hrsak@tuta.com>
2024-09-10 21:32:11 +0200

    enemy patrol and player detection finished
    done: enemy patrol, player detection and raycasting of detection cone

---
+++ Assets/Scripts/EnemyPatrolMove.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum NPCState
+{
+	random,
+	frozen,
+	distracted,
+	patrol
+}
+
+public class EnemyPatrolMove : MonoBehaviour
+{
+	public NPCState myState;
+	[SerializeField] NPCState defaultState;
+	NavMeshAgent myAgent;
+
+	[SerializeField] Transform[] patrolPositions;
+	[SerializeField] float waitAtPatrolSeconds, patrolStopDistance;
+	int indexPatrolPosition;
+
+	[SerializeField] Animator animator;
+
+	private void Start()
+	{
+		myAgent = GetComponent<NavMeshAgent>();
+		myAgent.updateRotation = true;
+		myState = defaultState;
+		Patrol();
+	}
+
+	private void Update()
+	{
+		//animator.SetFloat("Speed", myAgent.velocity.magnitude);
+		if (myState == NPCState.patrol)
+			Patrol();
+		if (myState == NPCState.frozen)
+		{
+			Vector3 lookVectorTowardsPatrolPosition = transform.position - patrolPositions[indexPatrolPosition].position;
+		}
+	}
+
+	private void Patrol()
+	{
+		if (Vector3.Distance(transform.position, patrolPositions[indexPatrolPosition].position) > patrolStopDistance)
+		{
+			myAgent.SetDestination(patrolPositions[indexPatrolPosition].position);
+			
+		}
+		else
+		{
+			myState = NPCState.frozen;
+			StartCoroutine(waitPatrolSpot());
+			indexPatrolPosition++;
+			if (patrolPositions.Length <= indexPatrolPosition)
+			{
+				indexPatrolPosition = 0;
+			}
+		}
+	}
+
+	private IEnumerator waitPatrolSpot()
+	{
+		yield return new WaitForSeconds(waitAtPatrolSeconds);
+		if (myState == NPCState.frozen)
+		{
+			myState = NPCState.patrol;
+			Patrol();
+		}
+	}
+}
